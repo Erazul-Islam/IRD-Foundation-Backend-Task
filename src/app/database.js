649cb144d68630e819c89d63ffdefa0c5dae8bc5
Erazul Islam <erazul89@gmail.com>
2024-11-21 21:45:44 +0600

    successfully deployed

--- src/app/database.js
+++ src/app/database.js
@@ -1,6 +1,8 @@
+const path = require('path')
 const sqlite3 = require('sqlite3').verbose();
 
-const dbPath = './dua.sqlite';
+const dbPath = path.join(__dirname, '../../dua.sqlite');
+
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
         console.error('Error connecting to database:', err.message);
